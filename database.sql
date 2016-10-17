cc8945b5df9392ec671db481a65d96af01922909
elisa lee <elisa.my.lee@gmail.com>
2016-10-17 08:41:39 -0500

    changes to database set up

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 -- setting up the tasks database
 CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
-    task_name varchar(80),
-    complete boolean
+    task_name varchar(80) NOT NULL,
+    complete BOOLEAN DEFAULT FALSE NOT NULL,
 );
